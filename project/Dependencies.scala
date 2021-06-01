0e91a8598bbbae392fdd63a49015e5d6a46bd108
Alexey Novakov <novakov.alex@gmail.com>
2021-06-01 23:19:08 +0200

    use java onnx version

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,5 +2,6 @@ import sbt._
 
 object Dependencies {
   lazy val onnxScala = "org.emergent-order" %% "onnx-scala-backends" % "0.15.0" withSources()
+  lazy val onnxJava  = "com.microsoft.onnxruntime" % "onnxruntime" % "1.7.0"
   lazy val opencv = "org.bytedeco" % "javacv-platform" % "1.5.5"
 }
--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -10,32 +10,66 @@ import org.bytedeco.javacpp.indexer.FloatRawIndexer
 import org.bytedeco.opencv.global.opencv_core.{divide, CV_32FC4}
 import org.bytedeco.opencv.global.opencv_imgproc.*
 
-import java.nio.file.{Files, Paths, Path}
+import ai.onnxruntime.{OrtEnvironment, OrtSession}
+import ai.onnxruntime.OnnxTensor
 
-val tensorShapeDenotation = "Batch" ##: "Height" ##: "Width" ##: "Channel" ##: TSNil
-val tensorDenotation: String & Singleton = "Image"
+import scala.jdk.CollectionConverters._
+import scala.util.Using
+import java.nio.file.{Files, Paths, Path}
+import java.nio.FloatBuffer
+
+val TensorShapeDenotation = "Batch" ##: "Height" ##: "Width" ##: "Channel" ##: TSNil
+val TensorDenotation: String & Singleton = "Image"
 val ImageWidth = 416
 val ImageHeight = 416
 val Channels = 3
-val gridSize: Dimension = 52
-val lastDim: Dimension = 255
+val GridSize: Dimension = 52
+val BoundingBoxes = 85
+val Anchors = 3
+val BoundingBoxesDim: Dimension = (BoundingBoxes * Anchors).asInstanceOf[Dimension]
+val ModelPath = Paths.get("models", "yolov3.onnx")
+
+val env = OrtEnvironment.getEnvironment()
+val session = env.createSession(ModelPath.toString, OrtSession.SessionOptions())
+
+enum YoloGrid(val name: String, val size: Int):
+  case Small   extends YoloGrid("conv_105", 52)
+  case Medium  extends YoloGrid("conv_93", 26)
+  case Large   extends YoloGrid("conv_81", 13)  
 
 def shape(batch: Int) =
   batch #: ImageHeight #: ImageWidth #: Channels #: SNil
 
-def getModel(path: Path = Paths.get("models", "yolov3.onnx")) =
+def getModel(path: Path = ModelPath) =
   val bytes = Files.readAllBytes(path)
   ORTModelBackend(bytes)
 
 def predict(images: Array[Float], model: ORTModelBackend, batch: Dimension = 1) =
-  val input = Tensor(images, tensorDenotation, tensorShapeDenotation, shape(batch))    
+  val input = Tensor(images, TensorDenotation, TensorShapeDenotation, shape(batch))    
   model.fullModel[
     Float, 
     "ImageClassification", 
-    "Batch" ##: "Class" ##: TSNil, 
-    batch.type #: gridSize.type #: gridSize.type #: lastDim.type #: SNil
+    "Batch" ##: "BoundingBoxes" ##: TSNil, 
+    batch.type #: GridSize.type #: GridSize.type #: BoundingBoxesDim.type #: SNil
   ](Tuple(input))
 
+def predict(images: Array[Float], batch: Long): Map[YoloGrid, Array[Float]] =     
+  var tensor = OnnxTensor.createTensor(env, FloatBuffer.wrap(images), Array(batch, ImageHeight, ImageWidth, Channels))
+  val inputs = Map("input_1:0" -> tensor).asJava
+  Using.resource(session.run(inputs)) { result =>
+    result.iterator().asScala.toList
+      .map{ r => 
+        val key =
+          if r.getKey == YoloGrid.Small.name then YoloGrid.Small
+          else if r.getKey == YoloGrid.Medium.name then YoloGrid.Medium
+          else YoloGrid.Large
+
+        key -> r.getValue.asInstanceOf[OnnxTensor].getFloatBuffer.array()
+      }
+      .toMap
+  }
+
+// TODO: clone initial image
 def toModelInput(image: Mat): Array[Float] =  
   resize(image, image, Size(ImageHeight, ImageWidth))
   toArray(scale(image))
@@ -67,8 +101,11 @@ def toArray(mat: Mat): Array[Float] =
 def main = 
   val file = Paths.get("zebra.jpg")  
   val image = imread(file.toString)
-  val input = toModelInput(image)
+  val input = toModelInput(image)  
   val model = getModel()
-  val out = predict(input, model)
-  println(out)
-  println(out.data.length)
+  val outputs = predict(input, 1)
+  
+  val smallGrid = outputs(YoloGrid.Small)
+    .grouped(BoundingBoxes).toArray
+    .grouped(Anchors).toArray
+    .grouped(YoloGrid.Small.size).toArray  
